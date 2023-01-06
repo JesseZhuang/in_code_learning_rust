e2d5cdd46d0c69cce82f105ea70701a4eda3eb7c
Jesse Zhuang <zhzexi@gmail.com>
2023-01-05 23:35:25 -0800

    tuples

--- src/data_type/primitive.rs
+++ src/data_type/primitive.rs
@@ -37,8 +37,15 @@ pub fn primitives() {
     println!("type of var mutable {} is {}", mutable, mutable.type_name_of());
 }
 
+fn reverse_tuple(pair: (i32, i32)) -> (i32, i32) {
+    let (v1, v2) = pair; // unpack, destructure to create bindings
+    (v2, v1)
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::data_type::primitive::reverse_tuple;
+
     #[test]
     fn test_literals() {
         assert_eq!(3, 1u32 + 2);
@@ -48,5 +55,15 @@ mod tests {
         assert_eq!(100_0000, 1_000_000u32);
         assert_eq!(12, 0o10 + 4);
         println!("rust void? unit type: {:?}", ())
+    }
+
+    #[test]
+    fn test_tuples() {
+        let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+        println!("{:?}", tuple_of_tuples);
+        assert_eq!(1, tuple_of_tuples.0.0);
+        assert_eq!(1, (1u32, ).0);
+        assert_eq!(1, (1u32));
+        assert_eq!((1, 2, ), reverse_tuple((2, 1)));
     }
 }
