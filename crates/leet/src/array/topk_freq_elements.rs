67c4aa39c8455fd216faf856712356ebe9014984
Jesse Zhuang <zhzexi@gmail.com>
2024-10-29 15:33:24 -0700

    lc practice

--- crates/leet/src/array/topk_freq_elements.rs
+++ crates/leet/src/array/topk_freq_elements.rs
@@ -4,11 +4,10 @@ impl Solution {
     pub fn top_k_frequent(nums: Vec<i32>, k: i32) -> Vec<i32> {
         let (mut count, N) = (HashMap::new(), nums.len());
         for &n in nums.iter() { *count.entry(n).or_insert(0) += 1; }
-        let mut buk = vec![Vec::new(); N];
-        for (&n, &cnt) in count.iter() { buk[N - cnt].push(n); }
-        buk.into_iter().flatten().take(k as usize).collect()
+        let mut buk = vec![Vec::new(); N + 1];
+        for (&n, &cnt) in count.iter() { buk[cnt].push(n); }
+        buk.into_iter().flatten().skip(count.len() - k as usize).collect()
     }
 }
 
 struct Solution;
-
