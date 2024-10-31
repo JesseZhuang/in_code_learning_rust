26729de9c53e0b4661549c095212d1c7be984374
Jesse Zhuang <zhzexi@gmail.com>
2024-10-31 00:37:04 -0700

    lc practice

---
+++ crates/leet/src/dp/min_distance_travel.rs
@@ -0,0 +1,21 @@
+// lc 2463
+
+impl Solution {
+    pub fn minimum_total_distance(mut rob: Vec<i32>, mut fac: Vec<Vec<i32>>) -> i64 {
+        rob.sort();
+        fac.sort();
+        let (n, m) = (rob.len(), fac.len());
+        let mut dp = vec![10i64.pow(12); n + 1]; // scientific notation
+        dp[0] = 0;
+        for f in fac.iter() {
+            for _ in 0..f[1] {
+                for (i, &r) in rob.iter().enumerate().rev() {
+                    dp[i + 1] = dp[i + 1].min((r - f[0]).abs() as i64 + dp[i]);
+                }
+            }
+        }
+        dp[n]
+    }
+}
+
+struct Solution;
--- crates/leet/src/dp/mod.rs
+++ crates/leet/src/dp/mod.rs
@@ -1,2 +1,3 @@
 mod lcss;
 mod regex_match;
+mod min_distance_travel;
