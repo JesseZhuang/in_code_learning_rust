d083c51ff15f6c132498cdf03d783a7e55408b27
Jesse Zhuang <zhzexi@gmail.com>
2024-11-14 17:01:45 -0800

    lc practice

--- crates/rust/src/data_type/string.rs
+++ crates/rust/src/data_type/string.rs
@@ -10,4 +10,23 @@ mod tests {
         }
         assert_eq!('h', s.chars().next().unwrap());
     }
+
+    #[test]
+    fn test_init() {
+        let s_ref: &str = "hello_r"; // a reference to string, string slice, immutable
+        assert_eq!("Hello_r", s_ref.replace("h", "H")); // generate new string
+        let mut s_owned = String::from("hello_o"); // owns the data
+        s_owned.push('.');
+        s_owned.push_str(s_ref);
+        assert_eq!("hello_o.hello_r", s_owned);
+    }
+
+    #[test]
+    fn get_slice() {
+        let slice = "hello";
+        assert_eq!(String::from("hello"), slice);
+        assert_eq!("hello", slice);
+        assert_eq!("hel", &slice[0..3]);
+        assert_eq!("h", &slice[0..1]); // to index a string slice
+    }
 }
